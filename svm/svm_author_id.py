5749f3ccdae97182ac11c592c87ffd9c56c8fc44
abhisekshukla <shukla.abhisek@gmail.com>
2019-02-24 22:59:47 +0530

    mods for svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
 
 
 ### features_train and features_test are the features for the training
@@ -19,12 +20,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
 
+clf = SVC(kernel='linear')
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
-#########################################################
-### your code goes here ###
+t0 = time()
+clf.predict(features_test);
+print "predict time:", round(time()-t0, 3), "s"
 
-#########################################################
+print("accuricy: ",clf.score(features_test, labels_test))
 
 
